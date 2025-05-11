535067fc5e7e0c21f3c3c5f59e073950364d77a1
euteajudo <suporte@euteajudoalicitar.com.br>
2025-05-11 19:58:48 -0300

    adiciona scripts `lint:check`e ]´lint:fix`

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1d8f3889537b22c7"; }
+/******/ 		__webpack_require__.h = function() { return "6437c8b90fa4d77c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu te Ajudo a Licitar</h1>
+  return <h1>minha primeia linha de codigo em REACT</h1>;
 }
 
 export default Home;
