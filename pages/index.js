416f6c4770dc1e2ab7ca3ad5ce75e83692aa6e26
euteajudo <suporte@euteajudoalicitar.com.br>
2025-05-11 22:23:59 -0300

    pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>minha primeia linha de codigo em REACT</h1>;
+  return (
+    <div>
+      <h1>Eu te Ajudo a Licitar</h1>
+      <p>Página em construção.</p>
+    </div>
+  );
 }
 
 export default Home;
